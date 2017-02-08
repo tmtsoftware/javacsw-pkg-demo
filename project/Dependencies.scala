4520fcf434d219f2832f082602783e112c50e11d
abrighton <78px3WT5s>
2017-02-08 22:44:29 +0100

    Updated to Scala-2.12.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
 
-  val Version = "0.3-SNAPSHOT"
-  val ScalaVersion = "2.11.8"
+  val Version = "0.4-SNAPSHOT"
+  val ScalaVersion = "2.12.1"
 
   val pkg = "org.tmt" %% "pkg" % Version
   val ccs = "org.tmt" %% "ccs" % Version
   val javacsw = "org.tmt" %% "javacsw" % Version
   val containerCmd = "org.tmt" %% "containercmd" % Version
-  val jeromq = "org.zeromq" % "jeromq" % "0.3.5"
+  val jeromq = "org.zeromq" % "jeromq" % "0.3.6"
 }
 
